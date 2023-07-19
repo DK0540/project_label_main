7aed32427796ee11701ba6a89be253b95eb734a9
Tutorial-git <dk0540@digikull.com>
2023-07-19 15:21:16 +0530

    updated

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,14 +9,31 @@ const HomePage = () => {
       <Link to="/app">
         <button className="button-3">Create Label Button</button>
       </Link>
-      <Link to="/labels">
-        <button>All label</button>
-      </Link>
-      <Link to="/allcards">
-        <button>All Cards</button>
-      </Link>
     </div>
   );
 };
 
 export default HomePage;
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "./homepage.css";
+
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to the Home Page</h1>
+//       <Link to="/app">
+//         <button className="button-3">Create Label Button</button>
+//       </Link>
+//       <Link to="/labels">
+//         <button>All label</button>
+//       </Link>
+//       <Link to="/allcards">
+//         <button>All Cards</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
