62b16ee7596a3cec4c025c558b38da3dc47789ee
vishwa_naik <94664943+DK0540@users.noreply.github.com>
2023-09-20 07:01:51 +0530

    Update ImgCards.js

--- src/components/ImgCards.js
+++ src/components/ImgCards.js
@@ -16,42 +16,6 @@ const ImgCards = () => {
 
 export default ImgCards;
 //===============================================================>>ok2
-// import React from "react";
-// import { Link } from "react-scroll";
-// import AnimatedCard from "./AnimatedCard";
-// import img1 from "./images/bar2.jpg";
-// const ImgCards = () => {
-//   return (
-//     <div className="imgcard">
-//       <AnimatedCard imageSrc={img1} text="Hovered Text" />
-//     </div>
-//   );
-// };
 
-// export default ImgCards;
-//============================================================================>>>ok2
-// import React from "react";
-// import { Link } from "react-scroll";
-// import "./imgcards.css";
 
-// const ImgCards = () => {
-//   return (
-//     <div>
-//       <Link to="our-work" smooth={true} duration={500}>
-// <div className="imgt">
-//   <h1>Our work</h1>
-// </div>
-//         <div className="imggrid">
-//           <div> a</div>
-//           <div>b</div>
-//           <div>c</div>
-//           <div>d</div>
-//           <div>e</div>
-//           <div>f</div>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// };
 
-// export default ImgCards;
